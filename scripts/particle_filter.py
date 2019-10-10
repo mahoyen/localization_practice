1d282d98c36a060521000903c48429996a60bf2b
mahoyen <32592450+mahoyen@users.noreply.github.com>
2019-10-10 23:38:28 +0100

    added other resampling formula

--- scripts/particle_filter.py
+++ scripts/particle_filter.py
@@ -41,15 +41,28 @@ def probabilty_after_measurement_on_list(list_of_robots, measurements, noises):
     print(sum_of_probs)
     return prob_list
 
-def resample_list_of_robots(list_of_robots, weights):
+def my_resample_list_of_robots(list_of_robots, weights):
     N = len(list_of_robots)
     new_robot_list = []
     cumulative_sum = np.cumsum(weights)
-    max_w = max(cumulative_sum)
     for _ in range(N):
         index = 0
-        beta = np.random.rand()*max_w
+        beta = np.random.rand()*cumulative_sum[-1]
         while beta > cumulative_sum[index]:
+            beta -= weights[index]
+            index = (index + 1 ) % N
+        new_robot_list.append(list_of_robots[index])
+    return new_robot_list
+
+def other_resample_list_of_robots(list_of_robots, weights):
+    N = len(list_of_robots)
+    new_robot_list = []
+    index = np.random.randint(N)
+    beta = 0.
+    max_w = max(weights)
+    for _ in range(N):
+        beta += np.random.rand()*2*max_w
+        while beta > weights[index]:
             beta -= weights[index]
             index = (index + 1 ) % N
         new_robot_list.append(list_of_robots[index])
@@ -59,7 +72,7 @@ def resample_list_of_robots(list_of_robots, weights):
 WORLD_SIZE = {'x': 100, 'y': 100}
 N_PARTICLES = 1000
 
-NOISE_STRAIGHT = 5
+NOISE_STRAIGHT = 50
 NOISE_SIDE = NOISE_STRAIGHT
 
 rotations = [0.0, 1.0, 3]
@@ -72,10 +85,9 @@ display_scatterplot(particles_of_robots, real_robot)
 for time_step in range(len(rotations)):
     real_robot.move(rotations[time_step], translations[time_step])
     noisy_meas = real_robot.sense(NOISE_STRAIGHT, NOISE_SIDE)
-    print(noisy_meas)
     particles_of_robots = move_motion_on_list_of_robots(particles_of_robots, rotations[time_step], translations[time_step])
     weights = probabilty_after_measurement_on_list(particles_of_robots, noisy_meas, [NOISE_STRAIGHT, NOISE_SIDE])
-    particles_of_robots = resample_list_of_robots(particles_of_robots, weights)
+    particles_of_robots = other_resample_list_of_robots(particles_of_robots, weights)
     
     display_scatterplot(particles_of_robots, real_robot)
 
